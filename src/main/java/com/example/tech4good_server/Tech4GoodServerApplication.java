3d25ce30c4e19c4a5cc779600c2ec2569a60961d
Jung Dayeon <102217402+al1kite@users.noreply.github.com>
2024-08-15 14:31:29 +0900

    [add] 프로젝트 생성

---
+++ src/main/java/com/example/tech4good_server/Tech4GoodServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.tech4good_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tech4GoodServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Tech4GoodServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/tech4good_server/Tech4GoodServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tech4good_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tech4GoodServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
