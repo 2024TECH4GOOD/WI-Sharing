59717211c454e4b124930216ef9732ad899e86c4
Jung Dayeon <102217402+al1kite@users.noreply.github.com>
2024-08-16 12:20:01 +0900

    [fix] FARM -> SENIOR

--- src/main/java/com/example/tech4good_server/global/model/enums/Role.java
+++ src/main/java/com/example/tech4good_server/global/model/enums/Role.java
@@ -6,7 +6,7 @@ import lombok.Getter;
 public enum Role {
 
     ADMIN("0", "ROLE_ADMIN"),// BackOffice 전체 Admin 관리 (개발팀)
-    FARM("100", "ROLE_SENIOR"), // 시니어
+    SENIOR("100", "ROLE_SENIOR"), // 시니어
     YOUTH("200", "ROLE_YOUTH"); // 자립 준비 청년
 
     private final String value;
