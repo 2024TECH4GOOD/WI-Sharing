379950d375db6a8f250a403bcb2d12949803ae70
Jung Dayeon <102217402+al1kite@users.noreply.github.com>
2024-08-21 20:04:56 +0900

    [fix] 질문 카테고리 변경

--- src/main/java/com/example/tech4good_server/global/model/enums/QuestionCategory.java
+++ src/main/java/com/example/tech4good_server/global/model/enums/QuestionCategory.java
@@ -1,7 +1,11 @@
 package com.example.tech4good_server.global.model.enums;
 
 public enum QuestionCategory {
-    FINANCE,
-    EMPLOYMENT,
-    EXTRA,
+    법,
+    금융,
+    경제,
+    주거,
+    노무,
+    기타
+
 }
